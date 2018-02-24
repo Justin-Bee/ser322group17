b57a84c7f348012c730c27d8bddbbfa086c8a84a
Justin Bee <jbee2@asu.edu>
2018-02-24 08:53:54 -0500

    Add files via upload

---
+++ addresource.php
@@ -0,0 +1,91 @@
+<html>
+
+<head>
+
+<title>Add Resource</title>
+
+</head>
+
+<body>
+
+<form action = "http://localhost:8080/resourceadded.php" method = "post">
+
+
+<b> Add a New Conference Room</b>
+
+
+<p>Room Number:<input type = "text" name = "room_number" size = "30" value = "" />
+
+</p>
+
+
+<p>Capacity:
+<input type = "text" name = "capacity" size = "30" value = "" />
+
+</p>
+
+
+
+<p>
+<input type ="submit" name="submit" value = "Send" />
+
+</p>
+
+
+</form>
+
+<form action = "http://localhost:8080/resourceadded.php" method = "post">
+
+
+<b> Add a New Laptop</b>
+
+
+<p>Device ID:<input type = "text" name = "device_id" size = "30" value = "" />
+
+</p>
+
+
+<p>Manufacturer:
+<input type = "text" name = "manufacturer" size = "30" value = "" />
+
+</p>
+
+
+
+<p>
+<input type ="submit" name="submit" value = "Send" />
+
+</p>
+
+
+</form>
+
+<form action = "http://localhost:8080/resourceadded.php" method = "post">
+
+
+<b> Add a New Projector</b>
+
+
+<p>Device ID:<input type = "text" name = "device_id" size = "30" value = "" />
+
+</p>
+
+
+<p>Lamp Hours:
+<input type = "text" name = "lamp_hours" size = "30" value = "" />
+
+</p>
+
+
+
+<p>
+<input type ="submit" name="submit" value = "Send" />
+
+</p>
+
+
+</form>
+
+</body>
+
+</html>
