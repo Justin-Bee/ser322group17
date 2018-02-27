2191852167d15a1861d7e442e708aae3d020c5c8
Justin Bee <jbee2@asu.edu>
2018-02-27 13:32:30 -0500

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -10,7 +10,7 @@ $port=3306;
 $socket="";
 $user="group17";
 $password="";
-$dbname="group17DB";
+$dbname="group17db";
 
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
